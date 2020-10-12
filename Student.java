91fd153364eac7e622ca0a17d2d70affa9731cc9
taanveerahmeed <taanveerahmeed@gmail.com>
2020-10-12 09:36:46 +0600

    Student modified and Course created

--- Student.java
+++ Student.java
@@ -1,4 +1,30 @@
 public class Student{
-	int sId;
-	String sName;
+	private String sName;
+	private String sId;
+	
+	public Student(){}
+	public Student(String sName, String sId){
+		this.sName = sName;
+		this.sId = sId;
+	}
+	public void setName(String sName){
+		this.sName = sName;
+	}
+	public void setId(String sId){
+		this.sId = sId;
+	}
+	public String getName(){
+		return sName;
+	}
+	public String getId(){
+		return sId;
+	}
+	public void showInfo(){
+		System.out.println("Name: " + sName);
+		System.out.println("Id: " + sId);
+		
+	}
+	
+	
+	
 }
